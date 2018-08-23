813eed9347adc23ae476a88e1cdc3bb578375209
Bolzon <blzn@mail.ru>
2018-08-23 19:51:42 -0300

    Adding basic structure of tests for user

---
+++ test/users.js
@@ -0,0 +1,23 @@
+
+const assert = require('assert');
+
+describe('Users', () => {
+
+	describe('.create()', () => {
+		it('should create a new user', async done => {
+			done(new Error('not implemented'));
+		});
+	});
+
+	describe('.delete()', () => {
+		it('should delete an existing user', async done => {
+			done(new Error('not implemented'));
+		});
+	});
+
+	describe('.update()', () => {
+		it('should update an existing user', async done => {
+			done(new Error('not implemented'));
+		});
+	});
+});
